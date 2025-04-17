afeae010f55a565494dbf891b0bb81157aaadd93
Sahasanu <sanusaha351@gmail.com>
2025-04-17 20:33:26 +0530

    bug fixes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function About() {
   return (
-    <div className='about_container  text-white mt-[5vh] lg:mt-[10vh]'>
+    <div className='about_container  text-white mt-[3vh] lg:mt-[10vh]'>
       <div className='font-[700] text-[30px]  md:text-4xl text-[#e6a420] w-full sm:w-11/12 md:w-10/12  pl-[5vw] '>About Me</div>
-      <div className="content w-[100%] pl-[15px] pr-[15px] lg:w-[90%] lg:ml-auto lg:mr-auto h-[30vh] overflow-scroll pt-[3vh] lg:overflow-auto lg:text-[20px]">👋 Hey there! I'm a passionate Web Developer with a strong foundation in building responsive, user-friendly websites and applications. I completed my 12th in 2023 from Jawahar Navodaya Vidyalaya School and am currently pursuing Data Science at Haldia Institution Of Technology College. Alongside coding, I'm a tech enthusiast at heart — always exploring new tools, frameworks, and trends that push the boundaries of what's possible on the web. My goal is to grow into a skilled, responsible engineer who not only builds great products but also makes a positive impact through technology.</div>
+      <div className="content w-[95%] pl-[20px] pr-[15px] lg:w-[90%] ml-auto mr-auto h-[30vh] lg:h-auto lg:pb-[3vh] overflow-scroll pt-[3vh] lg:overflow-auto lg:text-[20px] bg-[#76b5f927] rounded-[10px] mt-[1vh] lg:mt-[2vh]">👋 Hey there! I'm a passionate Web Developer with a strong foundation in building responsive, user-friendly websites and applications. I completed my 12th in 2023 from Jawahar Navodaya Vidyalaya School and am currently pursuing Data Science at Haldia Institution Of Technology College. Alongside coding, I'm a tech enthusiast at heart — always exploring new tools, frameworks, and trends that push the boundaries of what's possible on the web. My goal is to grow into a skilled, responsible engineer who not only builds great products but also makes a positive impact through technology.</div>
     </div>
   )
 }
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const footer = () => {
   return (
-    <div className='footer_container  text-center text-white h-[5vh] mt-[5vh]'>
-      <p className='font-bold text-[18px]'>&copy;Sanu Saha</p>
-    </div>
+    <div className='footer_container flex items-center justify-center text-center text-white h-[5vh] lg:mt-[5vh] mt-[3vh] bg-[#fcfafa1a]'>
+      <p className='font-bold text-[18px] '>&copy;Sanu Saha</p>
+    </div> 
   )
 }
 
